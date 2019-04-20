7844c0b4c7faef42682fe07440320e2b06990078
Thao Nguyen Duy <ngduythao2805@outlook.com>
2019-04-20 17:47:46 +0700

    test routes

--- proj-customer/routes/index.js
+++ proj-customer/routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {});
+});
+
+router.get('/cart', function(req, res, next) {
+  res.render('cart', {});
+});
+
+router.get('/checkout', function(req, res, next) {
+  res.render('checkout', {});
+});
+
+router.get('/product', function(req, res, next) {
+  res.render('product', {});
+});
+
+router.get('/shop', function(req, res, next) {
+  res.render('shop', {});
 });
 
 module.exports = router;
