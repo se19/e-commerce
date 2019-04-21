865400adf43b57fa51e3efe2d5a3a3efc06bbacf
le van thi <thi174hcmus@gmail.com>
2019-04-22 04:31:38 +0700

    Git sync - Add admin site layouts fully

---
+++ proj-admin/routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router
+  .get('/', (req, res, next) => {
+    res.render('index', {
+      title: 'Express'
+    });
+  })
+  .get('/products', (req, res, next) => {
+    res.render('products');
+  })
+  .get('/groups', (req, res, next) => {
+    res.render('groups');
+  })
+  .get('/orders', (req, res, next) => {
+    res.render('orders');
+  })
+  .get('/accounts', (req, res, next) => {
+    res.render('accounts');
+  })
+  .get('/statistics', (req, res, next) => {
+    res.render('statistics');
+  })
+  .get('/best-sales', (req, res, next) => {
+    res.render('best-sales');
+  })
+  .get('/login', (req, res, next) => {
+    res.render('login');
+  });
+
+module.exports = router;
