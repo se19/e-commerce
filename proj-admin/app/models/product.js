2109f7cd435dd666cf92cd6a46cd4ab3f273f17b
Thao Nguyen Duy <ngduythao2805@outlook.com>
2019-05-20 21:48:00 +0700

    Add model

---
+++ proj-admin/app/models/product.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  productId: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  importDate: {
+    type: Date,
+    required: true
+  },
+  brandId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Brand',
+    required: true
+  },
+  categoryId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  },
+  numberInventory: {
+    type: Number,
+    required: true
+  },
+  numberPurchased: {
+    type: Number,
+    default: 0,
+    required: true
+  },
+  rating: {
+    reviews: [{
+      reviewId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Rate',
+      },
+    }]
+  },
+  average: {
+    type: Number,
+    default: 0,
+    require: true
+  }
+});
+
+module.exports = mongoose.model('Product', productSchema);
--- proj-customer/models/product.js
+++ proj-customer/models/product.js
@@ -19,57 +19,51 @@ const productSchema = new Schema({
     description: {
         type: String,
         required: true
+    },
+    importDate: {
+        type: Date,
+        required: true
+    },
+    brandId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Brand',
+        required: true
+    },
+    categoryId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
+    numberInventory: {
+        type: Number,
+        required: true
+    },
+    numberPurchased: {
+        type: Number,
+        default: 0,
+        required: true
+    },
+    rating: {
+        comment: [{
+            reviewer: {
+                type:String,
+                required: true
+            },
+            info: {
+                type: String,
+                required: true
+            },
+            star: {
+                type: Number,
+                required: true
+            }
+        }],
+        average: {
+            type: Number,
+            default: 0,
+            require: true
+        }
     }
-    // image: [{
-    //     name: {
-    //         type: String,
-    //         required: true
-    //     }
-    // }],
-    // importDate: {
-    //     type: Date,
-    //     required: true
-    // },
-    // brandId: {
-    //     type: Schema.Types.ObjectId,
-    //     ref: 'Brand',
-    //     required: true
-    // },
-    // categoryId: {
-    //     type: Schema.Types.ObjectId,
-    //     ref: 'Category',
-    //     required: true
-    // },
-    // numberInventory: {
-    //     type: Number,
-    //     required: true
-    // },
-    // numberPurchased: {
-    //     type: Number,
-    //     default: 0,
-    //     required: true
-    // },
-    // rating: {
-    //     comment: [{
-    //         reviewer: {
-    //             type:String,
-    //             required: true
-    //         },
-    //         info: {
-    //             type: String,
-    //             required: true
-    //         },
-    //         star: {
-    //             type: Number,
-    //             required: true
-    //         }
-    //     }],
-    //     average: {
-    //         type: Number,
-    //         default: 0,
-    //         require: true
-    //     }
-    // }
 });
 
 module.exports = mongoose.model('Product', productSchema);
