9670fc7ca36e34dc9f81248c8c38223a06909bd5
le van thi <thi174hcmus@gmail.com>
2019-05-21 10:45:09 +0700

    connect to db - proj-admin

---
+++ proj-admin/config/dbs.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const conn_str = "mongodb+srv://admin:123@cluster0-llp1b.mongodb.net/shop?retryWrites=true";
+
+mongoose.set('useCreateIndex', true);
+
+const connect = (conn_str) => {
+    return mongoose.connect(conn_str, {
+            useNewUrlParser: true
+        })
+        .then(result => {
+            // console.log(result);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+
+exports.initdb = async () => {
+    await connect(conn_str);
+};
