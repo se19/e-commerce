bf1dfaf16228c191161bce46a677136a0323aba6
le van thi <thi174hcmus@gmail.com>
2019-06-03 23:45:19 +0700

    impl order management

--- proj-admin/constants/index.js
+++ proj-admin/constants/index.js
@@ -2,7 +2,11 @@ const constants = {
     DEFAULT_IMG: "images/default/no-image.png",
     DEFAULT_CUSTOMER_PASSWORD: "user",
     USERTYPE_ADMIN: "admin",
-    USERTYPE_CUSTOMER: "customer"
+    USERTYPE_CUSTOMER: "customer",
+    ORDER_STATUS_PENDING: "pending",
+    ORDER_STATUS_SHIPPING: "shipping",
+    ORDER_STATUS_COMPLETED: "completed",
+    ORDER_STATUS_CANCELED: "canceled"
 }
 
 module.exports = constants;
--- proj-admin/routes/orders.js
+++ proj-admin/routes/orders.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+const auth_controller = require('../app/controllers/auth-controller');
 const order_controller = require('../app/controllers/order-controller');
 
 router
-  .get('/', order_controller.list_order)
-  .post('/add', order_controller.create_order)
-  .put('/add', order_controller.update_order)
-  .get('/info', order_controller.get_order)
-  .delete('/info', order_controller.delete_order)
+  //check auth
+  .use('/', auth_controller.checkAuth)
+  //List
+  .get('/', order_controller.list_orders)
+  //Get create view
+  // .get('/add', order_controller.create_order)
+  //Create
+  // .post('/add', order_controller.create_order)
+  //Get info
+  .get('/:orderId', order_controller.get_order)
+  //Update
+  // .post('/:orderId/edit', order_controller.update_order)
+  .post('/:orderId/change-status', order_controller.change_order_status)
+  .post('/:orderId/cancel', order_controller.cancel_order)
+  .post('/:orderId/edit-user', order_controller.update_userinfo_order)
+  //Delete
+  .post('/:orderId/delete', order_controller.delete_order)
 
 module.exports = router;
